172e73829a38d3c61f6fb0f6ffb402913594af77
Bukay Mamadani <bukaymamadani@gmail.com>
2026-05-28 16:07:20 +0300

    updated flask message

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+return from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bukay Cloud DevOps App Running!"
+    return "Day 14 CI/CD Update Running"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
